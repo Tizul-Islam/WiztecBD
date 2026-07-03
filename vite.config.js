ad48543e69443ea8f26bb66f7d4179478c408723
tizul-islam <tizulislamtt@gmail.com>
2026-07-04 02:31:16 +0600

    feat: implement rollup code splitting for build optimization and configure Vercel rewrite rules for SPA routing

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('scheduler')) {
+              return 'react-vendor';
+            }
+            if (id.includes('@mui') || id.includes('@emotion')) {
+              return 'mui-vendor';
+            }
+            if (id.includes('firebase')) {
+              return 'firebase-vendor';
+            }
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
+
